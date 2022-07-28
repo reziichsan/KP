0045dd722457fed029609c65b13edba6cedac212
reziichsan <77427119+reziichsan@users.noreply.github.com>
2022-07-28 07:18:25 +0700

    Add files via upload

---
+++ anothermysite/members/views.py
@@ -0,0 +1,46 @@
+from pyexpat.errors import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from .models import Members
+
+def index(request):
+  mymembers = Members.objects.all().values()
+  template = loader.get_template('index.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+def add(request):
+  template = loader.get_template('add.html')
+  return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+  x = request.POST['first']
+  z = request.POST['messages']
+  member = Members(firstname=x, messages=z)
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
+
+def delete(request, id):
+  member = Members.objects.get(id=id)
+  member.delete()
+  return HttpResponseRedirect(reverse('index'))
+
+def update(request, id):
+  mymember = Members.objects.get(id=id)
+  template = loader.get_template('update.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+def updaterecord(request, id):
+  first = request.POST['first']
+  messages = request.POST['messages']
+  member = Members.objects.get(id=id)
+  member.name = first
+  member.messages = messages
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
